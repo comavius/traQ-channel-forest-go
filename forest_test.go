48d9644a4c32b717f7867b55243653bce25dffc8
comavius <comavius@proton.me>
2024-04-06 14:13:55 +0900

    passed tests

---
+++ forest_test.go
@@ -0,0 +1,46 @@
+package traqforest_test
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"testing"
+	traqforest "traq-forest"
+
+	"github.com/joho/godotenv"
+	"github.com/traPtitech/go-traq"
+)
+
+func TestNewForest(t *testing.T) {
+	// load ACCESS_TOKEN from .env
+	err := godotenv.Load()
+	if err != nil {
+		t.Fatal(err)
+	}
+	// create api_client
+	api_key := traq.APIKey{
+		Prefix: "Bearer",
+		Key:    os.Getenv("ACCESS_TOKEN"),
+	}
+	api_conf := traq.NewConfiguration()
+	api_conf.DefaultHeader["Authorization"] = fmt.Sprintf("%s %s", api_key.Prefix, api_key.Key)
+	api_client := traq.NewAPIClient(api_conf)
+	// create context
+	ctx := context.Background()
+	// create forest
+	forest := traqforest.NewForest(api_client, &ctx)
+	// get all channels
+	channels_request := api_client.ChannelApi.GetChannels(ctx)
+	channels, _, err := channels_request.Execute()
+	if err != nil {
+		t.Fatal(err)
+	}
+	// get all channels path
+	for _, channel := range channels.Public {
+		path, ok := forest.GetPath(channel.Id)
+		if !ok {
+			t.Fatal("GetPath failed")
+		}
+		fmt.Println(path)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,13 @@
+module traq-forest
+
+go 1.21.1
+
+require (
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/traPtitech/go-traq v0.0.0-20240224021219-538059ee2fa7 // indirect
+	golang.org/x/net v0.17.0 // indirect
+	golang.org/x/oauth2 v0.10.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
+)
